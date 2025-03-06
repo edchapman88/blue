7a37cde1eb39c859b4c4ef61bd76bb0093c33d13
Ed Chapman - Turing <edchapmanelc@gmail.com>
2025-03-06 20:15:03 +0000

    working udp integration test (ought to be removed from unit tests due to dependency on environment)

--- blue/receiver.ml
+++ blue/receiver.ml
@@ -1,20 +1,25 @@
 open Domainslib
 
-let serial addr =
+let serial addr () =
   print_endline "serial fn";
   match addr with
   | Unix.ADDR_INET _ -> failwith "unreachable"
   | Unix.ADDR_UNIX dev -> Serial.read dev
 
-let udp addr =
-  let fd = Unix.socket ~cloexec:false PF_INET SOCK_DGRAM 0 in
+let udp_sock = ref None
+
+let init_udp addr =
+  let fd = Unix.socket PF_INET SOCK_DGRAM 0 in
   Unix.bind fd addr;
-  (*Unix.listen fd 1000;*)
-  let buf_len = 1024 in
+  udp_sock := Some fd
+
+let udp addr () =
+  if Option.is_none !udp_sock then init_udp addr;
+  let sock = Option.get !udp_sock in
+  let buf_len = 1 in
   let buf = Bytes.create buf_len in
-  let len, _src = Unix.recvfrom fd buf buf_len 0 [] in
-  Unix.close fd;
-  if len = 0 then None else Some (Bytes.sub buf 0 len)
+  let len, _src = Unix.recvfrom sock buf 0 buf_len [] in
+  if len = 0 then None else Some buf
 
 let all_received chan =
   let rec aux acc =
@@ -27,28 +32,26 @@ let all_received chan =
 let relevant = ref []
 
 let ok_rate window_secs msg_chan () =
+  let start_win = Unix.gettimeofday () -. window_secs in
   let buf = !relevant @ all_received msg_chan in
   let relevant', num_ok =
     List.fold_left
       (fun (rel, count) (n_at_t, t) ->
-        if t < Unix.gettimeofday () -. window_secs then (rel, count)
+        if t < start_win then (rel, count)
         else ((n_at_t, t) :: rel, count + n_at_t))
       ([], 0) buf
   in
   relevant := List.rev relevant';
   float_of_int num_ok /. window_secs
 
-let listener_of_addr addr =
+let reader_of_addr addr =
   match addr with
-  | Unix.ADDR_UNIX _ -> serial
-  | Unix.ADDR_INET _ -> udp
+  | Unix.ADDR_UNIX _ -> serial addr
+  | Unix.ADDR_INET _ -> udp addr
 
-let listen ~listener chan =
+let listen ~reader chan =
   let rec loop () =
-    (*Unix.sleepf 0.0001;*)
-    (*let time = Unix.gettimeofday () in*)
-    (*if time -. Float.floor time < 0.001 then Printf.printf "time: %f\n" time;*)
-    match listener () with
+    match reader () with
     | None -> loop ()
     | Some msgs ->
         let time = Unix.gettimeofday () in
@@ -62,8 +65,7 @@ let listen ~listener chan =
   in
   loop ()
 
-let receiver_of_listener ~window_secs listener =
+let receiver_of_reader ~window_secs reader =
   let msg_chan = Chan.make_unbounded () in
-  let _listener = Domain.spawn (fun () -> listen ~listener msg_chan) in
-  (*Chan.send msg_chan (Unix.gettimeofday ());*)
+  let _listener = Domain.spawn (fun () -> listen ~reader msg_chan) in
   ok_rate window_secs msg_chan
--- test/receiver.ml
+++ test/receiver.ml
@@ -1,27 +1,36 @@
-let print_rate rate_fn = Printf.printf "OK rate = %.0f\n%!" (rate_fn ())
+open Blue
 
-let mock_send ~rate msg_chan =
+let rounded = true
+
+let print_rate rate_fn =
+  let rate = if rounded then rate_fn () |> Float.round else rate_fn () in
+  Printf.printf "OK rate = %.0f\n%!" rate
+
+(* [mock_sender ~rate chan] sends mock data over the channel [chan] at a constant [rate], indefinitely. *)
+let mock_sender ~rate msg_chan =
   let period = 1. /. rate in
   let rec loop () =
     Unix.sleepf period;
     let time = Unix.gettimeofday () in
+    (* [(1, time)] simulates 1 OK signal received at [time]. *)
     Domainslib.Chan.send msg_chan (1, time);
     loop ()
   in
   loop ()
 
+(* Spawn a domain in which [mock_sender] will run. The channel [msg_chan] receives mock data at a constant rate and is passed to [Receiver.ok_rate] to return a 'getter' for the average rate. *)
 let mock_receiver ~window_secs ~send_rate =
   let msg_chan = Domainslib.Chan.make_unbounded () in
   let _mock_sender =
-    Domain.spawn (fun () -> mock_send ~rate:send_rate msg_chan)
+    Domain.spawn (fun () -> mock_sender ~rate:send_rate msg_chan)
   in
   Blue.Receiver.ok_rate window_secs msg_chan
 
 let%expect_test "test rate evaluation" =
   (* A mock serial receiver evaluating an average OK rate with a look-back period of 4 seconds. The mock data has a constant OK rate of 10 per second. Hence the mock receiver should evaluate an average OK rate of 10. *)
-  let rate_getter = mock_receiver ~window_secs:4.0 ~send_rate:10.0 in
+  let rate_getter = mock_receiver ~window_secs:3.0 ~send_rate:10.0 in
   (* Warmup period of 5 seconds (longer than the lookback period of 4 seconds). *)
-  Unix.sleep 5;
+  Unix.sleep 4;
   print_rate rate_getter;
   Unix.sleep 1;
   print_rate rate_getter;
@@ -30,25 +39,16 @@ let%expect_test "test rate evaluation" =
   OK rate = 10
   |}]
 
-(*let%expect_test "udp" =*)
-(*let addr = Unix.ADDR_INET (Unix.inet_addr_loopback, 8081) in*)
-(*let rate_fn = Blue.Receiver.receiver 3.0 addr in*)
-(*print_rate rate_fn;*)
-(*Unix.sleep 1;*)
-(*print_rate rate_fn;*)
-(*[%expect {| |}]*)
-
-(* Assuming the listener is called in quite a tight loop (with little computation between calls), [mock_listener] will return appoximately 10 OK signals every second: an OK rate of 10. *)
-let mock_listener () =
+(* Mocks a reader (e.g. a serial or UDP reader), returning [bytes option]. Assuming the reader is called in quite a tight loop (with little computation between calls), [mock_reader] will return appoximately 10 OK signals (note: a value of '1' is an OK signal and a value of '0' is an ERR signal) every second: an OK rate of 10. *)
+let mock_reader () =
   Unix.sleepf 0.2;
   Some (Bytes.of_string "10010")
 
 let%expect_test "test listening loop" =
-  let window_secs = 4.0 in
-  let rate_getter =
-    Blue.Receiver.receiver_of_listener ~window_secs mock_listener
-  in
-  Unix.sleep 5;
+  (* Test the (tight) listening loop that is spawned in a sperate OS thread by [Receiver.receiver_of_reader], using [mock_reader]. *)
+  let window_secs = 3.0 in
+  let rate_getter = Receiver.receiver_of_reader ~window_secs mock_reader in
+  Unix.sleep 4;
   print_rate rate_getter;
   Unix.sleep 1;
   print_rate rate_getter;
@@ -57,9 +57,22 @@ let%expect_test "test listening loop" =
     OK rate = 10
     |}]
 
-(*let%expect_test "single udp" =*)
-(*let addr = Unix.ADDR_INET (Unix.inet_addr_any, 8081) in*)
-(*(match Blue.Receiver.udp addr () with*)
-(*| None -> print_endline "none"*)
-(*| Some _ -> print_endline "some");*)
-(*[%expect {| |}]*)
+let%expect_test "udp" =
+  let window_secs = 3.0 in
+  let addr = Unix.ADDR_INET (Unix.inet_addr_loopback, 8081) in
+  let reader = Receiver.reader_of_addr addr in
+  let rate_fn = Receiver.receiver_of_reader ~window_secs reader in
+  Unix.sleep 4;
+  print_rate rate_fn;
+  Unix.sleep 1;
+  print_rate rate_fn;
+  Unix.sleep 1;
+  print_rate rate_fn;
+  Unix.sleep 1;
+  print_rate rate_fn;
+  [%expect {|
+    OK rate = 19
+    OK rate = 19
+    OK rate = 19
+    OK rate = 18
+    |}]
