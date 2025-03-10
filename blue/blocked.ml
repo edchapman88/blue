cca06bc830222d1f61128bc00dc0022e5bce0c47
Ed Chapman - Turing <edchapmanelc@gmail.com>
2025-03-10 13:21:03 +0000

    remove sudo and fix toggle (unblock if blocked)

--- blue/blocked.ml
+++ blue/blocked.ml
@@ -16,22 +16,16 @@ let blocked_of_string msg =
   matches regex msg |> BlockedSet.of_list
 
 let query_blocked () =
-  sys_cmd "/run/wrappers/bin/sudo nft list set inet filter blocked"
-  |> blocked_of_string
+  sys_cmd "nft list set inet filter blocked" |> blocked_of_string
 
 let toggle_block ip =
   let exit_code =
     if BlockedSet.mem ip @@ query_blocked () then
       Sys.command
-      @@ Printf.sprintf
-           "/run/wrappers/bin/sudo nft 'add element inet filter blocked { %s }'"
-           ip
+      @@ Printf.sprintf "nft 'delete element inet filter blocked { %s }'" ip
     else
       Sys.command
-      @@ Printf.sprintf
-           "/run/wrappers/bin/sudo nft 'delete element inet filter blocked { \
-            %s }'"
-           ip
+      @@ Printf.sprintf "nft 'add element inet filter blocked { %s }'" ip
   in
   if exit_code = 0 then Ok ()
   else Error (Printf.sprintf "Failed to toggle the block on IP address: %s" ip)
