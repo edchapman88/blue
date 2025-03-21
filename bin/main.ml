a44b7387d9e49a03d4d10ed18ff591f2eff32c0f
Ed Chapman - Turing <edchapmanelc@gmail.com>
2025-03-21 14:35:34 +0000

    add documentation to main.ml

--- bin/main.ml
+++ bin/main.ml
@@ -1,10 +1,16 @@
+(* Blue Executable: Defending a web-server.
+
+   Commence a [Markov.Agent.S.act] loop to make observations of, and take actions in, the cyber system.
+
+   A module of type [Markov.Agent.S] is created using the functor [Markov.Agent.Make]. The functor has three arguments (each a module) which must be implementations of [Markov.Agent.MarkovCompressorType], [Markov.Agent.RewardType] and [Markov.Agent.RLPolicyType].
+
+   The [blue] library has an implementation for each of these module types, namely [Blue.MarkovCompressor], [Blue.Reward] and [Blue.CountBasedPolicy], respectively.
+*)
+
 open Blue
 
 let () = Cli.arg_parse ()
 
-module Policy = CountBasedPolicy
-module Agent = Markov.Agent.Make (MarkovCompressor) (Reward) (Policy)
+module Agent = Markov.Agent.Make (MarkovCompressor) (Reward) (CountBasedPolicy)
 
-let () =
-  Random.init 0;
-  Agent.act (Policy.init ())
+let () = Agent.act (CountBasedPolicy.init ())
